311476158bc4e4f748d1a725e30a875bbe5a079c
BIT <BIT@DESKTOP-DOGVD1T>
2018-03-22 14:35:38 +0900

    3_add_master

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -7,6 +7,8 @@ public class HelloGit {
 		System.out.println("1_hello_git");
 		//1. hotfix 추가 후 마스터 브렌치 수정 
 		System.out.println("2_add_hotfix_git");
+		//2. marge 후 마스터 브랜치 수정 !!
+		System.out.println("3_add_master");
 	}
 
 }
